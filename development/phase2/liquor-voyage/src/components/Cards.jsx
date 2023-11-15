d992926b2753fd4cb5b547c8fd77ccaa5fa876c5
Tracy <tracy.kagiri@moringa.student.com>
2023-11-15 12:23:32 +0300

    initial commit

--- development/phase2/liquor-voyage/src/components/Cards.jsx
+++ development/phase2/liquor-voyage/src/components/Cards.jsx
@@ -18,9 +18,9 @@ export default function Offers() {
   }, []);
 
   return (
-    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6 '>
       {offers.map((offer) => (
-        <div key={offer.id} className='rounded-xl relative my-4'>
+        <div key={offer.id} className='rounded-xl relative my-4 hover:scale-105 duration-300'>
           {/* overlay */}
           <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
             <p className='font-bold text-2xl px-2 pt-4'>{offer.name}</p>
